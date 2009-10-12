e82e784629de4e4b7ad8d0a8dd2a12cc6b3c65c8
more.andres <more.andres@e80ff990-b4f2-11de-aa42-85eb4131a9e0>
2009-10-12 02:05:03 +0000

    added libtool configuration

---
+++ trunk/inc/mpiskel.h
@@ -0,0 +1,32 @@
+/*
+ * mki-skel - header interface
+ *
+ * Copyright 2009, Andres More <more.andres@gmail.com>
+ *
+ * This program is free software, you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef MPISKEL_H
+#define MPISKEL_H
+
+/**
+ * DOC: Introduction
+ *
+ * mpi-skel is a skeleton for MPI applications. This document implementes 
+ * the framework and defines the callbacks to be implemented.
+ *
+ */
+
+/**
+ * DOC: Warning
+ *
+ * If you are reading this as a document and not the header file itself, it
+ * will be incomplete because not all documentation has been converted yet.
+ */
+
+  
+  
+
+#endif /* MPISKEL_H */
