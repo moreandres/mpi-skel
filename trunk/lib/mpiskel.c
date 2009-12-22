70cf99fa283c1cbf4831c07787f5035d6a275b68
more.andres@gmail.com <more.andres@gmail.com@e80ff990-b4f2-11de-aa42-85eb4131a9e0>
2009-12-22 02:07:20 +0000

    Added sample code sketches

--- trunk/lib/mpiskel.c
+++ trunk/lib/mpiskel.c
@@ -152,7 +152,9 @@ static pipeline_t * pipeline_create(stage_t ** stages, int count, int options) {
   assert(stages != NULL);
   assert(count > 0);
   assert(options >= 0);
-
+  
+  i = 1;
+  
   pipeline = calloc(1, sizeof(pipeline_t));
   
   return pipeline;
@@ -228,37 +230,67 @@ static int timestamp(char * buffer, int size) {
 }
 
 typedef struct log_s {
+  int fd;
+  char * name;
 } log_t;
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define FILENAME_SIZE 64
 
 static log_t * log_create(int options) {
   
   log_t * log = NULL;
-  
+
   assert(options >= 0);
+  
+  log = calloc(1, sizeof(log_t));
+  
+  log->name = calloc(FILENAME_SIZE, sizeof(char));
+  log->fd = open(log->name, 0);
 
   return log;
 }
 
-static int log_close(log_t * log) {
+#include <unistd.h>
+
+static int log_destroy(log_t * log) {
 
   int result = -1;
 
   assert(log != NULL);
+
+  free(log);
+  log = NULL;
+
+  result = close(log->fd);
 
   return result;
 }
 
 typedef struct mpi_s {
   int rank;
-  int count;
-  int name;
+  int size;
+  char * name;
+  int length;
 } mpi_t;
 
-static mpi_t * mpi_init(int options) {
+#include "/usr/lib/openmpi/1.3.2-gcc/include/mpi.h"
+
+static mpi_t * mpi_init(int * argc, char ** argv[], int options) {
   
+  int result = -1;
   mpi_t * mpi = NULL;
   
   assert(options >= 0);
+
+  result = MPI_Init(argc, argv);
+  
+  result = MPI_Comm_rank(MPI_COMM_WORLD, &mpi->rank);
+  result = MPI_Comm_size(MPI_COMM_WORLD, &mpi->size);
+  result = MPI_Get_processor_name(mpi->name, &mpi->length);
   
   return mpi;
 }
@@ -269,6 +301,8 @@ static int mpi_finalize(mpi_t * mpi, int options) {
   
   assert(mpi != NULL);
   assert(options >= 0);
+  
+  result = MPI_Finalize();
   
   return result;
 }
--- trunk/src/matrix.c
+++ trunk/src/matrix.c
@@ -1,1 +1,67 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <mpiskel.h>
+#include <assert.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <time.h>
+#include <math.h>
+#include <sys/time.h>
+
+static double * mm_init(int n)
+{
+  assert(n > 0);
+
+  double * res = NULL;
+  
+  res = malloc(n * n * sizeof(double));
+
+  srandom(time(NULL));
+  
+  int i,j;
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++)
+      res[i * n + j] = 1.0 * random() / RAND_MAX;
+  
+  return res;
+}
+
+static int mm_simple(double * a, double * b, double * c, int n)
+{
+  assert(a != NULL && b != NULL && c !=NULL && n > 0);
+    
+  int i,j,k;
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++)
+      for (k = 0; k < n; k++)
+        c[i * n + j] += a[i * n + k] * b[k * n + j];
+
+  return 0;
+}
+
+double wtime(void)
+{
+  double sec;
+  struct timeval tv;
+  
+  gettimeofday(&tv,NULL);
+  sec = tv.tv_sec + tv.tv_usec/1000000.0;
+  return sec;
+}
+
+int mm_check(double * a, double * b, int n)
+{
+  assert(a != NULL && b != NULL && n > 0);
+  
+  int i,j;
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++)
+      {
+        if (fabs(a[i * n + j] - b[i * n + j]) > 0.000001)
+          {
+            return -1;
+          }
+      }
+  
+  return 0;
+}
