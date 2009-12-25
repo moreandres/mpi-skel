b1cb2a20391e3f3634c73be314259a7c9dd4039b
more.andres@gmail.com <more.andres@gmail.com@e80ff990-b4f2-11de-aa42-85eb4131a9e0>
2009-12-25 16:42:42 +0000

    Fixed argp handling, created pipeline.[ch]

--- lib/mpiskel.c
+++ lib/mpiskel.c
@@ -9,39 +9,29 @@
 
 int main(int argc, char *argv[])
 {
-	struct arguments arguments;
+	printd("%d, %p", argc, argv);
 
-	/* argp handler? */
-	arguments.verbose = 0;
-	arguments.file = "-";
+	assert(argc >= 0);
+	assert(argv != NULL);
 
-	argp_parse(&argp, argc, argv, 0, NULL, &arguments);
+	args = setup_argp(argc, argv);
 
-	/* signal handler? */
-
-	/* mpi initialization? */
+	print_args(args);
 
 	return 0;
 }
 
-typedef struct pipeline_s {
-} pipeline_t;
-
-typedef void *(*fn_t) (void *args);
-
-typedef struct stage_s {
-	fn_t fn;
-	int options;
-} stage_t;
-
 static void setup_signal_handler(void)
 {
+
+	printd("()");
 
 	return;
 }
 
 static void setup_mpi(void)
 {
+	printd("()");
 
 	return;
 }
@@ -49,115 +39,7 @@ static void setup_mpi(void)
 static void setup_driver(void)
 {
 
-	return;
-}
-
-static stage_t *stage_create(fn_t fn, int options)
-{
-
-	stage_t *stage = NULL;
-
-	assert(fn != NULL);
-	assert(options > 0);
-
-	stage = calloc(1, sizeof(stage_t));
-
-	stage->fn = fn;
-	stage->options = options;
-
-	return stage;
-}
-
-static int stage_destroy(stage_t *stage, int options)
-{
-
-	int result;
-
-	assert(stage != NULL);
-	assert(options >= 0);
-
-	free(stage);
-	stage = NULL;
-
-	return result;
-}
-
-static int stage_execute(stage_t *stage, int options)
-{
-
-	int result = 1;
-
-	assert(stage != NULL);
-	assert(options >= 0);
-
-	return result;
-}
-
-static void stage_show(stage_t *stage)
-{
-
-	assert(stage != NULL);
-
-	return;
-}
-
-static pipeline_t *pipeline_create(stage_t **stages, int count, int options)
-{
-
-	pipeline_t *pipeline = NULL;
-	int i = 0;
-
-	assert(stages != NULL);
-	assert(count > 0);
-	assert(options >= 0);
-
-	i = 1;
-
-	pipeline = calloc(1, sizeof(pipeline_t));
-
-	return pipeline;
-}
-
-static int pipeline_destroy(pipeline_t *pipeline, int options)
-{
-
-	int result = -1;
-
-	assert(pipeline != NULL);
-	assert(options >= 0);
-
-	return result;
-}
-
-static int pipeline_start(pipeline_t *pipeline, int options)
-{
-
-	int result = -1;
-
-	assert(pipeline != NULL);
-	assert(options >= 0);
-
-	return result;
-}
-
-static int pipeline_stop(pipeline_t *pipeline, int options)
-{
-
-	int result = -1;
-
-	assert(pipeline != NULL);
-	assert(options >= 0);
-
-	return result;
-}
-
-static void pipeline_show(pipeline_t *pipeline, int options)
-{
-
-	printd("%p, %d", pipeline, options);
-
-	assert(pipeline != NULL);
-	assert(options >= 0);
+	printd("()");
 
 	return;
 }
@@ -178,6 +60,8 @@ static log_t *log_create(int options)
 
 	log_t *log = NULL;
 
+	printd("%d", options);
+
 	assert(options >= 0);
 
 	log = calloc(1, sizeof(log_t));
@@ -194,6 +78,8 @@ static int log_destroy(log_t *log)
 {
 
 	int result = -1;
+
+	printd("%p", log);
 
 	assert(log != NULL);
 
@@ -220,6 +106,10 @@ static mpi_t *mpi_init(int *argc, char **argv[], int options)
 	int result = -1;
 	mpi_t *mpi = NULL;
 
+	printd("%p, %p, %d", argc, argv, options);
+
+	assert(argc != NULL);
+	assert(argv != NULL);
 	assert(options >= 0);
 
 	result = MPI_Init(argc, argv);
@@ -235,6 +125,8 @@ static int mpi_finalize(mpi_t *mpi, int options)
 {
 
 	int result = -1;
+
+	printd("%p, %d", mpi, options);
 
 	assert(mpi != NULL);
 	assert(options >= 0);
---
+++ lib/pipeline.c
@@ -0,0 +1,126 @@
+#include "pipeline.h"
+
+stage_t *stage_create(fn_t fn, int options)
+{
+
+	stage_t *stage = NULL;
+
+	printd("%p, %d", fn, options);
+
+	assert(fn != NULL);
+	assert(options > 0);
+
+	stage = calloc(1, sizeof(stage_t));
+
+	stage->fn = fn;
+	stage->options = options;
+
+	return stage;
+}
+
+int stage_destroy(stage_t *stage, int options)
+{
+
+	int result;
+
+	printd("%p, %d", stage, options);
+
+	assert(stage != NULL);
+	assert(options >= 0);
+
+	free(stage);
+	stage = NULL;
+
+	return result;
+}
+
+int stage_execute(stage_t *stage, int options)
+{
+
+	int result = 1;
+
+	printd("%p, %d", stage, options);
+
+	assert(stage != NULL);
+	assert(options >= 0);
+
+	return result;
+}
+
+void stage_show(stage_t *stage)
+{
+	printd("%p", stage);
+
+	assert(stage != NULL);
+
+	return;
+}
+
+pipeline_t *pipeline_create(stage_t **stages, int count, int options)
+{
+
+	pipeline_t *pipeline = NULL;
+	int i = 0;
+
+	printd("%p, %d, %d", stages, count, options);
+
+	assert(stages != NULL);
+	assert(count >= 0);
+	assert(options >= 0);
+
+	i = 1;
+
+	pipeline = calloc(1, sizeof(pipeline_t));
+
+	return pipeline;
+}
+
+int pipeline_destroy(pipeline_t *pipeline, int options)
+{
+
+	int result = -1;
+
+	printd("%p, %d", pipeline, options);
+
+	assert(pipeline != NULL);
+	assert(options >= 0);
+
+	return result;
+}
+
+int pipeline_start(pipeline_t *pipeline, int options)
+{
+
+	int result = -1;
+
+	printd("%p, %d", pipeline, options);
+
+	assert(pipeline != NULL);
+	assert(options >= 0);
+
+	return result;
+}
+
+int pipeline_stop(pipeline_t *pipeline, int options)
+{
+
+	int result = -1;
+
+	printd("%p, %d", pipeline, options);
+
+	assert(pipeline != NULL);
+	assert(options >= 0);
+
+	return result;
+}
+
+void pipeline_show(pipeline_t *pipeline, int options)
+{
+
+	printd("%p, %d", pipeline, options);
+
+	assert(pipeline != NULL);
+	assert(options >= 0);
+
+	return;
+}
