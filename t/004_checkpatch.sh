65de27611e4db7a3241afc349a0e04979effc7bd
more.andres@gmail.com <more.andres@gmail.com@e80ff990-b4f2-11de-aa42-85eb4131a9e0>
2009-12-29 01:05:28 +0000

    Fixed t/004_checkpatch.sh

--- t/004_checkpatch.sh
+++ t/004_checkpatch.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-for file in `find .. -name "*.[ch]"`; do 
-    checkpatch.pl --no-tree --terse --strict --file $file; 
+for file in `find .. -name "*.[ch]"`; do
+    checkpatch.pl --no-tree --terse --strict --file $file | grep ERROR
+    [ $? -gt 0 ] || exit 1 
 done
