c198f801840cc2519bae9d3b724182b6733fcd18
more.andres@gmail.com <more.andres@gmail.com@e80ff990-b4f2-11de-aa42-85eb4131a9e0>
2010-02-08 22:01:24 +0000

    Added new interface

--- inc/pipe.h
+++ inc/pipe.h
@@ -1,13 +1,30 @@
 #ifndef PIPE_H
 #define PIPE_H
 
+#define STAGE_MAX 32
+
 typedef struct pipe_data_s *pipe_data_t;
+
+typedef struct stage_s {
+	char *name;
+	void *(*work) (void *);
+	int options;
+	void *(*pre) (void *);
+	void *(*post) (void *);
+	pipe_data_t data;
+} stage_t;
+
 typedef struct pipe_s {
+	char *name;
+	int options;
+	stage_t stages[STAGE_MAX];
 	pipe_data_t data;
 	int (*create) (void);
 	int (*destroy) (void);
 } pipe_t;
 
 pipe_t *get_pipe(void);
+
+extern pipe_t *get(void);
 
 #endif /* PIPE_H */
