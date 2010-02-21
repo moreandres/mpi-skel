8c16a79a17ddb3b20c42ae269d0e87af61defd53
more.andres@gmail.com <more.andres@gmail.com@e80ff990-b4f2-11de-aa42-85eb4131a9e0>
2010-02-21 01:35:22 +0000

    only try mpi if required

--- trunk/lib/mpi.c
+++ trunk/lib/mpi.c
@@ -13,29 +13,27 @@ static mpi_t *_mpi;
 
 static int mpi_init(int *argc, char **argv[])
 {
-
-	int result = -1;
-	mpi_t *mpi = NULL;
+	int error = 0;
 
 	printd("(%p, %p)", argc, argv);
 
 	assert(argc != NULL);
 	assert(argv != NULL);
 
-	result = MPI_Init(argc, argv);
+	error = MPI_Init(argc, argv);
 
-	mpi = calloc(1, sizeof(mpi_t));
-
-	result = MPI_Comm_rank(MPI_COMM_WORLD, &mpi->rank);
-	result = MPI_Comm_size(MPI_COMM_WORLD, &mpi->size);
+	if (!error) {
+		error = MPI_Comm_rank(MPI_COMM_WORLD, &_mpi->rank);
+		error = MPI_Comm_size(MPI_COMM_WORLD, &_mpi->size);
 
 #define HOSTNAME_SIZE 64
 
-	mpi->name = calloc(HOSTNAME_SIZE, sizeof(char));
+		_mpi->name = calloc(HOSTNAME_SIZE, sizeof(char));
 
-	result = MPI_Get_processor_name(mpi->name, &mpi->length);
+		error = MPI_Get_processor_name(_mpi->name, &_mpi->length);
+	}
 
-	return 0;
+	return error;
 }
 
 static int mpi_finalize(void)
