1bfe913865ed5f28d3a879ed8dc3c630af06997b
more.andres@gmail.com <more.andres@gmail.com@e80ff990-b4f2-11de-aa42-85eb4131a9e0>
2013-07-12 19:43:17 +0000

    updated

--- trunk/src/heat.c
+++ trunk/src/heat.c
@@ -1,149 +1,84 @@
-/*
- * This program calculates heat distribution in parallel and outputs
- * data to be processed by gnuplot's pm3d.
- *
- * bugsto: more.andres@gmail.com
- */
-
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>		/* memcpy, memset */
-#include <assert.h>
+#include <stdlib.h>
 #include <math.h>
-#include <mpiskel.h>
 
-#include "utils.h"
+#define CRESN 702	/* x, y resolution */
+#define RESN 700
+#define MAX_COLORS 8
+#define MAX_HEAT 20.0
 
-/* Constants */
+double solution[2][CRESN][CRESN], diff_constant;
+int cur_gen, next_gen;
 
-#define BASE 10			/* strtol */
-#define MAX_ITER 50000		/* upper bound */
-#define LENGTH 100              /* square grid size */
+double sim_time, final = 10000.0, time_step = 0.1, diff = 10000.0;
 
-#define at(x, y) ((x) * LENGTH + (y))
+void compute_one_iteration(void);
+void setup(void);
 
-typedef struct data_s {
-	double *grid;
-	double *tmp;
-	int bytes;
-} data_t;
-
-static int setup(void *params)
+int main(int argc, char *argv[])
 {
-	printd("(%p)", params);
+	int temp;
 
-	data_t * data = (data_t *) params;
+	/* set window position */
 
-	data->bytes = sizeof(double) * LENGTH * LENGTH;
-	assert(data->bytes > 0);
+	/* Calculate and draw points */
 
-	/* Allocate grid and helper */
-	data->grid = calloc(1, data->bytes);
-	assert(data->grid != NULL);
+	setup();
 
-	data->tmp = calloc(1, data->bytes);
-	assert(data->tmp != NULL);
-
-	return 0;
-}
-
-static inline double update(double *tmp, int i, int j)
-{
-	return 0.25 *
-		(tmp[at(i, j - 1)]
-		 + tmp[at(i, j + 1)]
-		 + tmp[at(i - 1, j)]
-		 + tmp[at(i + 1, j)]);
-}
-
-static inline double difference(double *grid, double *tmp, int i, int j)
-{
-	return fabs(grid[at(i, j)] - tmp[at(i, j)]);
-}
-
-static int work(void *params)
-{
-	printd("(%p)", params);
-
-	data_t *data = (data_t *) params;
-	double *grid = data->grid;
-	double *tmp = data->tmp;
-
-	int bytes = data->bytes;
-
-	/* problem details */
-	int error = 10;
-	int x = 5;
-	int y = 5;
-	int heat = 50;
-
-	double diff = RAND_MAX;
-
-	int i = -1;
-	int j = -1;
-	int k = -1;
-
-	/* Start threading support to avoid delays on each iteration */
-	{
-		/* Loop until reducing error or reaching maximum iterations */
-		for (k = 0; k < MAX_ITER && diff > error; k++) {
-
-			/* Create grid copy and place heat */
-			memcpy(&tmp[0], &grid[0], bytes);
-			tmp[at(x, y)] = heat;
-
-			error = 0;
-			double gap = -1;
-
-			/* Parallel heat distribution */
-			for (i = 1; i < LENGTH; i++) {
-				for (j = 1; j < LENGTH; j++) {
-
-					/* Update using neighbours average */
-					grid[at(i, j)] = update(tmp, i, j);
-
-					/* Update max heat difference */
-					gap = difference(grid, tmp, i, j);
-					if (diff < gap) {
-						diff = gap;
-					}
-				}
-			}
-		}
+	for (sim_time = 0; sim_time < final; sim_time += time_step) {
+		compute_one_iteration();
+		temp = cur_gen;
+		cur_gen = next_gen;
+		next_gen = temp;
 	}
 
-	grid[at(x, y)] = heat;
-
 	return 0;
 }
 
-static int save(void *params)
+void setup()
 {
-	printd("()");
+	int i, j;
 
-	/*
-	  for (i = 0; i < length; i++) {
-	  for (j = 0; j < length; j++) {
-	  printf("%d %d %f\n", i, j, grid[at(i, j)]);
-	  }
-	  printf("\n");
-	  }
-	*/
+#pragma omp parallel for shared(solution) private(i,j)
+	for (i = 0; i < CRESN; i++)
+		for (j = 0; j < CRESN; j++)
+			solution[0][i][j] = solution[1][i][j] = 0.0;
 
-	return 0;
+	cur_gen = 0;
+	next_gen = 1;
+	diff_constant = diff * time_step / ((float)RESN * (float)RESN);
+
 }
 
-static pipe_t heat = {
-	.name = "heat",
-	.stages = {
-		{ "setup", setup, 0, NULL, NULL },
-		{ "work", work, 0, NULL, NULL },
-		{ "save", save, 0, NULL, NULL },
-		{},
-	},
-};
-
-pipe_t *get(void)
+void compute_one_iteration(void)
 {
-	return &heat;
+	int i, j;
+	/* set boundary values */
+	for (i = 0; i < CRESN; i++) {
+		if (i < 256 || i > 768)
+			solution[cur_gen][i][0] = solution[cur_gen][i][1];
+		else
+			solution[cur_gen][i][0] = MAX_HEAT;
+	}
+	for (i = 0; i < CRESN; i++) {
+		solution[cur_gen][i][CRESN - 1] =
+		    solution[cur_gen][i][CRESN - 2];
+	}
+	for (i = 0; i < CRESN; i++) {
+		solution[cur_gen][0][i] = solution[cur_gen][1][i];
+		solution[cur_gen][CRESN - 1][i] =
+		    solution[cur_gen][CRESN - 2][i];
+	}
+	/* corners ? */
+
+#pragma omp parallel for shared(solution,cur_gen,next_gen,diff_constant) private(i,j)
+	for (i = 1; i <= RESN; i++)
+		for (j = 1; j <= RESN; j++)
+			solution[next_gen][i][j] = solution[cur_gen][i][j] +
+			    (solution[cur_gen][i + 1][j] +
+			     solution[cur_gen][i - 1][j] +
+			     solution[cur_gen][i][j + 1] +
+			     solution[cur_gen][i][j - 1] -
+			     4.0 * solution[cur_gen][i][j]) * diff_constant;
+
 }
