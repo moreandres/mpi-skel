4f328fbdde47bca71a7061016790fd1d26680f01
more.andres@gmail.com <more.andres@gmail.com@e80ff990-b4f2-11de-aa42-85eb4131a9e0>
2013-07-12 19:43:17 +0000

    updated

--- src/matrix.c
+++ src/matrix.c
@@ -1,82 +1,29 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <assert.h>
-#include <unistd.h>
-#include <syslog.h>
-#include <time.h>
-#include <math.h>
-#include <sys/time.h>
+#define SIZE 2048
 
-#include <mpiskel.h>
+float a[SIZE][SIZE];
+float b[SIZE][SIZE];
+float c[SIZE][SIZE];
 
-#include "utils.h"
-
-#define SIZE 100
-
-typedef struct data_s {
-	double *a;
-	double *b;
-	double *c;
-	int n;
-} data_t;
-
-static int setup(void *data)
+int main()
 {
-	printd("()");
-
-	data = calloc(1, sizeof(data_t));
-
-	data_t *data2 = (data_t *) &data;
-
-	printf("AM2: %p\n", data2);
-
-	data2->a = calloc(SIZE * SIZE, sizeof(double));
-	data2->b = calloc(SIZE * SIZE, sizeof(double));
-	data2->c = calloc(SIZE * SIZE, sizeof(double));
-	data2->n = SIZE;
-
-	srandom(time(NULL));
-
-	int i, j;
-
-	for (i = 0; i < data2->n; i++)
-		for (j = 0; j < data2->n; j++)
-			data2->a[i * SIZE + j] = 1.0 * random() / RAND_MAX;
-
-	for (i = 0; i < data2->n; i++)
-		for (j = 0; j < data2->n; j++)
-			data2->b[i * SIZE + j] = 1.0 * random() / RAND_MAX;
-
-	return 0;
-}
-
-static int work(void *data)
-{
-	printd("()");
-
-	data_t *data2 = (data_t *) data;
-	printf("AM3: %p\n", data2);
-
 	int i, j, k;
-	for (i = 0; i < data2->n; i++)
-		for (j = 0; j < data2->n; j++)
-			for (k = 0; k < data2->n; k++)
-				data2->c[i * SIZE + j] += data2->a[i * SIZE + k]
-				    * data2->b[k * SIZE + j];
+
+	for (i = 0; i < SIZE; ++i) {
+		for (j = 0; j < SIZE; ++j) {
+			a[i][j] = (float)i + j;
+			b[i][j] = (float)i - j;
+			c[i][j] = 0.0f;
+		}
+	}
+
+#pragma omp parallel for shared(a,b,c) 
+	for (i = 0; i < SIZE; ++i) {
+		for (j = 0; j < SIZE; ++j) {
+			for (k = 0; k < SIZE; ++k) {
+				c[i][j] += a[i][k] * b[k][j];
+			}
+		}
+	}
 
 	return 0;
-}
-
-static pipe_t matrix = {
-	.name = "matrix",
-	.stages = {
-		{"setup", setup, 0, NULL, NULL},
-		{"work", work, 0, NULL, NULL},
-		{},
-	},
-};
-
-pipe_t *get(void)
-{
-	return &matrix;
 }
